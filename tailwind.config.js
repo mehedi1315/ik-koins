c294529a55d9b20ee43c7c46d67689c6f56ba6aa
mehedi1315 <mehediahsankhulna@gmail.com>
2022-04-07 12:11:47 +0600

    project -Ik coin

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'pink-rose': '#dca378',
+
+        hug: {
+          hone:'#91C140',
+          htwo:'#05B2AF',
+          hblue:'#7B61FF',
+        },
+
+        sellfy: {
+          blue: '#3c97ef',
+          gray: '#F4F4F4',
+          green:'#00b140',
+          green2:'#08a122',
+        },
+
+        ikoins: {
+          yellow: '#ffc301',
+          yellowBdr: '#dfa300'
+        },
+      
+        orange: {
+          50: '#FFF7ED',
+          100: '#FFEDD5',
+          200: '#FED7AA',
+          300: '#FDBA74',
+          400: '#FB923C',
+          500: '#F97316',
+          600: '#EA580C',
+          700: '#C2410C',
+          800: '#9A3412',
+          900: '#7C2D12',
+        }
+        },
+    },
+  },
+  variants: {
+    extend: {
+       
+    },
+  },
+  plugins: [],
+}
